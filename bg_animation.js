052bd99547381bc16e2afc52d404587fe922d88f
ChocoTabe <50797128+ChocoTabe@users.noreply.github.com>
2022-06-02 22:02:50 +0900

    Update bg_animation.js

--- bg_animation.js
+++ bg_animation.js
@@ -8,6 +8,33 @@ $(() => {
     $("#main-music-title").text("Lost Way");
     $("#main-artist").text("ChocoTabe");
     $("#main-description").text(
+      `  ChocoTabe의 45번째 자작곡. 2022년에 만들어진 첫 곡이다. 
+      'Heart Fluttering'은 다시 시작하면서 걱정되는 마음도 있지만 설레이는 마음을 표현한 음악이다. 
+      Electronic 장르에 BPM 118로, 일반적인 템포이다. 
+      이 곡은 Lost Way 이후로 7개월이 지나고 나서야 만든 것으로 자작곡 발전에 있어서 의미가 있다. `
+    );
+    $("#play-now-button").attr("href", "play/HeartFluttering.html");
+    setTimeout(() => {
+      $("#main-music-title, #main-artist, #main-description").fadeOut();
+      if (window.innerWidth > 960) {
+        $("#bg1").animate({ right: 1920 }, duration, () => {
+          $("#bg1").css("right", -1920);
+        });
+      } else {
+        $("#bg1").animate({ right: 960 }, duration, () => {
+          $("#bg1").css("right", -960);
+        });
+      }
+      $("#bg2").animate({ left: 0 }, animationTime, () => {
+        secondCall();
+      });
+    }, delayTime);
+  }
+  function secondCall() {
+    $("#main-music-title, #main-artist, #main-description").fadeIn();
+    $("#main-music-title").text("Lost Way");
+    $("#main-artist").text("ChocoTabe");
+    $("#main-description").text(
       `  ChocoTabe의 44번째 자작곡. 2021년에 만들어진 마지막 곡이다. 'Lost Way'는
   잃어버린 길의 여정을 그린 것으로 몽환적인 분위기의 음악이다. Electronic 장르에
   BPM 128로, 몽환적인 분위기를 가진 음악치고는 템포가 느리지 않다. `
@@ -29,7 +56,7 @@ $(() => {
       });
     }, delayTime);
   }
-  function secondCall() {
+  function thirdCall() {
     $("#main-music-title, #main-artist, #main-description").fadeIn();
     $("#main-music-title").text("For You");
     $("#main-artist").text("ChocoTabe");
@@ -56,7 +83,7 @@ $(() => {
       });
     }, delayTime);
   }
-  function thirdCall() {
+  function forthCall() {
     $("#main-music-title, #main-artist, #main-description").fadeIn();
     $("#main-music-title").text("Like a Star");
     $("#main-artist").text("ChocoTabe");
@@ -81,30 +108,4 @@ $(() => {
         forthCall();
       });
     }, delayTime);
-  }
-  function forthCall() {
-    $("#main-music-title, #main-artist, #main-description").fadeIn();
-    $("#main-music-title").text("Regret");
-    $("#main-artist").text("ChocoTabe");
-    $("#main-description").text(
-      `  ChocoTabe의 41번째 자작곡. 'Regret'는 지난날들에 대한 후회를 하고 있는 것을
-  표현한 음악이다. Electronic 장르에 BPM 114로, 약간 느린 템포이다.`
-    );
-    $("#play-now-button").attr("href", "play/Regret.html");
-    setTimeout(() => {
-      $("#main-music-title, #main-artist, #main-description").fadeOut();
-      if (window.innerWidth > 960) {
-        $("#bg4").animate({ left: -1920 }, duration, () => {
-          $("#bg4").css("left", 1920);
-        });
-      } else {
-        $("#bg4").animate({ left: -960 }, duration, () => {
-          $("#bg4").css("left", 960);
-        });
-      }
-      $("#bg1").animate({ right: 0 }, animationTime, () => {
-        mainAnimation();
-      });
-    }, delayTime);
-  }
 });
