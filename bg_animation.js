a3be6185dc507f9f85ffb1d103678dd08b19fe0c
ChocoTabe <50797128+ChocoTabe@users.noreply.github.com>
2022-06-03 20:34:54 +0900

    Update bg_animation.js

--- bg_animation.js
+++ bg_animation.js
@@ -8,10 +8,7 @@ $(() => {
     $("#main-music-title").text("Heart Fluttering");
     $("#main-artist").text("ChocoTabe");
     $("#main-description").text(
-      `  ChocoTabe의 45번째 자작곡. 2022년에 만들어진 첫 곡이다. 
-      'Heart Fluttering'은 다시 시작하면서 걱정되는 마음도 있지만 설레이는 마음을 표현한 음악이다. 
-      Electronic 장르에 BPM 118로, 일반적인 템포이다. 
-      이 곡은 Lost Way 이후로 7개월이 지나고 나서야 만든 것으로 자작곡 발전에 있어서 의미가 있다. `
+      `  ChocoTabe의 45번째 자작곡. 2022년에 만들어진 첫 곡이다. 'Heart Fluttering'은 다시 시작하면서 걱정되는 마음도 있지만 설레이는 마음을 표현한 음악이다. Electronic 장르에 BPM 118로, 일반적인 템포이다. 이 곡은 Lost Way 이후로 7개월이 지나고 나서야 만든 것으로 자작곡 발전에 있어서 의미가 있다. `
     );
     $("#play-now-button").attr("href", "play/HeartFluttering.html");
     setTimeout(() => {
@@ -43,15 +40,15 @@ $(() => {
     setTimeout(() => {
       $("#main-music-title, #main-artist, #main-description").fadeOut();
       if (window.innerWidth > 960) {
-        $("#bg1").animate({ right: 1920 }, duration, () => {
-          $("#bg1").css("right", -1920);
+        $("#bg2").animate({ right: 1920 }, duration, () => {
+          $("#bg2").css("right", -1920);
         });
       } else {
-        $("#bg1").animate({ right: 960 }, duration, () => {
-          $("#bg1").css("right", -960);
+        $("#bg2").animate({ right: 960 }, duration, () => {
+          $("#bg2").css("right", -960);
         });
       }
-      $("#bg2").animate({ left: 0 }, animationTime, () => {
+      $("#bg3").animate({ left: 0 }, animationTime, () => {
         thirdCall();
       });
     }, delayTime);
@@ -66,33 +63,7 @@ $(() => {
   Like a Star에서 사용된 강렬한 전자기타 음이 들리는 것이
   특징이다.`
     );
-    $("#play-now-button").attr("href", "play/For You.html");
-    setTimeout(() => {
-      $("#main-music-title, #main-artist, #main-description").fadeOut();
-      if (window.innerWidth > 960) {
-        $("#bg2").animate({ left: -1920 }, duration, () => {
-          $("#bg2").css("left", 1920);
-        });
-      } else {
-        $("#bg2").animate({ left: -960 }, duration, () => {
-          $("#bg2").css("left", 960);
-        });
-      }
-      $("#bg3").animate({ left: 0 }, animationTime, () => {
-        forthCall();
-      });
-    }, delayTime);
-  }
-  function forthCall() {
-    $("#main-music-title, #main-artist, #main-description").fadeIn();
-    $("#main-music-title").text("Like a Star");
-    $("#main-artist").text("ChocoTabe");
-    $("#main-description").text(
-      `  ChocoTabe의 43번째 자작곡. 'Like a Star'는 전쟁터에서 영웅이 별처럼 나타나는
-  모습을 표현한 음악이다. Electronic 장르에 BPM 134으로, 약간 빠른 템포이다.
-  전자기타와 드럼이 강렬하게 들려 웅장한 느낌이 나는 것이 특징이다.`
-    );
-    $("#play-now-button").attr("href", "play/Like a Star.html");
+    $("#play-now-button").attr("href", "play/ForYou.html");
     setTimeout(() => {
       $("#main-music-title, #main-artist, #main-description").fadeOut();
       if (window.innerWidth > 960) {
@@ -105,6 +76,32 @@ $(() => {
         });
       }
       $("#bg4").animate({ left: 0 }, animationTime, () => {
+        forthCall();
+      });
+    }, delayTime);
+  }
+  function forthCall() {
+    $("#main-music-title, #main-artist, #main-description").fadeIn();
+    $("#main-music-title").text("Like a Star");
+    $("#main-artist").text("ChocoTabe");
+    $("#main-description").text(
+      `  ChocoTabe의 42번째 자작곡. 'Like a Star'는 전쟁터에서 영웅이 별처럼 나타나는
+  모습을 표현한 음악이다. Electronic 장르에 BPM 134으로, 약간 빠른 템포이다.
+  전자기타와 드럼이 강렬하게 들려 웅장한 느낌이 나는 것이 특징이다.`
+    );
+    $("#play-now-button").attr("href", "play/LikeaStar.html");
+    setTimeout(() => {
+      $("#main-music-title, #main-artist, #main-description").fadeOut();
+      if (window.innerWidth > 960) {
+        $("#bg4").animate({ left: -1920 }, duration, () => {
+          $("#bg4").css("left", 1920);
+        });
+      } else {
+        $("#bg4").animate({ left: -960 }, duration, () => {
+          $("#bg4").css("left", 960);
+        });
+      }
+      $("#bg1").animate({ left: 0 }, animationTime, () => {
         firstCall();
       });
     }, delayTime);
